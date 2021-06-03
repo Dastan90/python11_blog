1cc87f6372f009e0089d361d47a17e103700cc84
Dastan90 <tashybekov900510@gmail.com>
2021-06-03 17:21:19 +0600

    versia 1

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category
+
+
+def index_page(request):
+    categories = Category.objects.all()
+    return render(request, 'main/index.html', {'categories': categories})
+#TODO: Переписать при помощи классов
+
+#TODO: список постов по категориям
+#TODO: Переход по страницам
+#TODO: Регистрация, активация, логин, логаут
+#TODO: Фильтрация, поиск, сортировка
+#TODO: Пагинация
+#TODO: Переиспользование шаблонов
+#TODO: Проверка правов
+#TODO: Избранное
+#TODO: Дизайн
